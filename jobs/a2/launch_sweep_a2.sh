a53f4a45a9121482bc851e88f2a5a57e2d16b683
Vinicius Mello <viniciusdsmello@poli.ufrj.br>
2022-12-03 16:14:42 -0300

    Reorganize slurm jobs in sepate directories

---
+++ jobs/a2/launch_sweep_a2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks-per-node=1       # Number of tasks/node
+#SBATCH --partition=cpu           # The partion name: gpu or cpu
+#SBATCH --job-name=train_wnn_a2   # job name
+#SBATCH --exclusive               # Reserve this node only for you
+#SBATCH --account=vinicius.mello  # account name
+
+
+cd $HOME/Workspace/Projects/weightless-neural-networks
+
+singularity exec \
+    --env-file $PWD/.env \
+    -B $PWD:/app \
+    weightless-neural-networks_latest.sif \
+    python assignments/a2/sweep.py --sweep 
